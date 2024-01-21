26bc82187978bbd04c96f3339efd229d85024851
Umaru Biango <usbiango@gmail.com>
2024-01-21 21:57:30 +0400

    First commit after initilizing remote repository

--- src/App.js
+++ src/App.js
@@ -1,25 +1,136 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './Style.css';
 
-function App() {
+function TodoControls({onClearCompletedTask, onClearAllTask}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='tada-wrapper'>
+      <button className='btn' onClick={onClearAllTask}>Clear all task</button>
+      <button className='btn' onClick={onClearCompletedTask}>
+      Clear completed
+      </button>
     </div>
+  )
+}
+
+function TaskList({tasks, onToggleTask}) {
+  return (
+    <ul className='task-list-ul'>
+      {tasks.map((task, index) => (
+        <li key={index} className='task-list-li'>
+          <input
+              type='checkbox'
+              id={`task-checkbox-${index}`}
+              className='task-checkbox'
+              checked={task.completed}
+              onChange={() => onToggleTask(index)}
+            /> 
+          <label htmlFor={`task-checkbox-${index}`} className='task-checkbox-label'>
+           {' '} {task.text}
+          </label>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+function AddTask(props) {
+
+  const {inputValue, handleChange, handleSubmit} = props
+
+  return (
+    <form className='add-task'>
+      <input
+        type='text'
+        value={inputValue}
+        onChange={handleChange}
+        placeholder='Enter task...'
+        className='add-task-input'
+      />
+      <button 
+        onClick={handleSubmit}
+        className='add-task-button'>Add Task
+      </button>
+    </form>
+  )
+}
+
+function Header() {
+  return (
+    <h1 className='header'>Ta-Da List App</h1>
+  )
+}
+
+
+function ToDoContainer() {
+
+  const [tasks, setTasks] = useState([])
+  const [inputValue, setInputValue] = useState('')
+  const [checkedTask, setCheckedTask] = useState([])
+
+  function handleChange(e) {
+    setInputValue(e.target.value)
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    if (inputValue.trim() !== '') {
+      setTasks([...tasks, {text: inputValue, completed: false}])
+      setInputValue('')
+    } else {
+      alert('Please enter task')
+    }
+  }
+
+  function handleToggleTask(index) {
+    const updatedTasks = tasks.map((task, i) => 
+      i === index ? {...task, completed: !task.completed} : task
+    )
+    setTasks(updatedTasks)
+
+    if (checkedTask.includes(index)) {
+      setCheckedTask(checkedTask.filter((i) => i !== index))
+    } else {
+      setCheckedTask(checkedTask.concat(index))
+    }
+  }
+
+  function handleClearCompletedTask() {
+    let completedTask = tasks.filter((_, index) => !checkedTask.includes(index))
+    setTasks(completedTask)
+    setCheckedTask([])
+  }
+
+  function handleClearAllTask() {
+    tasks.splice(0, tasks.length)
+    setTasks([...tasks])
+  }
+
+
+  return (
+    <div className='container'>
+      <Header />
+      <AddTask 
+        inputValue={inputValue}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+      />
+      <TaskList
+        tasks={tasks}
+        onToggleTask={handleToggleTask}
+      />
+      <TodoControls
+        onClearCompletedTask={handleClearCompletedTask}
+        onClearAllTask={handleClearAllTask}
+      />
+    </div>
+  )
+}
+
+function ToDoApp() {
+  return (
+    <ToDoContainer />
   );
 }
 
-export default App;
+export default ToDoApp;
